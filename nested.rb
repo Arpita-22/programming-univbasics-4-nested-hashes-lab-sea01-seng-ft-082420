8d24ca2fb85e97809fbba9c6d1d65339ad9896fd
Arpita Dutta <duttaarpita22@gmail.com>
2020-07-22 00:11:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,10 +87,11 @@ def adding_matz
       }
     }
     
-    programmer_hash[0][:yukihiro_matsumoto] = {
+    hash = {:yukihiro_matsumoto => {
    :known_for => "Ruby",
    :languages => ["LISP", "C"]
    }
+    }
    
    programmer_hash
 
